8d0df7f9ca728dd93e7984ae258077062c49fe8f
343033429 <343033429@MXL1502T7G.tdsb.on.ca>
2024-09-24 10:45:05 -0400

    AboutMe

---
+++ ICS3U-StewartK/src/unit1/AboutMe.java
@@ -0,0 +1,17 @@
+package unit1;
+/**
+ * Description: This program prints out hello world and hi
+ * Date: Sept. 19, 2024
+ * @author Kenny Stewart
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		System.out.println("Kenny Stewart");
+		System.out.println("I am in Grade 11");
+		System.out.println("My teacher is Mrs. Kemp");
+		System.out.println("I go to Lawrence Park");
+		System.out.println("\"Go Panthers!\"");
+	}
+
+}

f10dfcec40a90977aa14b9bf6c3d1d101c557437
343033429 <343033429@MXL1502T7G.tdsb.on.ca>
2024-09-25 10:29:11 -0400

    school timetable

---
+++ ICS3U-StewartK/src/unit1/Schedule.java
@@ -0,0 +1,53 @@
+package unit1;
+
+public class Schedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Timetable for week of September 23");
+		System.out.println("________________________________________________________________________");
+		System.out.println("Monday");
+		System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:00-10:15 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Accounting", "10:20-11:40 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Biology", "12:40-1:55 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Functions", "2:00-3:15 \n");
+		
+		System.out.println("________________________________________________________________________");
+		System.out.println("Tuesday");
+		System.out.format("Class: %-16s | Time: %8s", "Accounting", "9:00-10:15 \n");
+		System.out.format("Class: %-15s | Time: %8s", "Computer Science", "10:20-11:40 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Functions", "12:40-1:55 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Biology", "2:00-3:15 \n");
+		
+		System.out.println("________________________________________________________________________");
+		System.out.println("Wednesday");
+		System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:55-10:55 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Accounting", "11:00-12:00 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Biology", "1:00-2:05 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Functions", "2:10-3:15 \n");
+		
+		System.out.println("________________________________________________________________________");
+		System.out.println("Thursday");
+		System.out.format("Class: %-16s | Time: %8s", "Accounting", "9:00-10:15 \n");
+		System.out.format("Class: %-15s | Time: %8s", "Computer Science", "10:20-11:40 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Functions", "12:40-1:55 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Biology", "2:00-3:15 \n");
+		
+		System.out.println("________________________________________________________________________");
+		System.out.println("Friday");
+		System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:00-10:15 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Accounting", "10:20-11:40 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Biology", "12:40-1:55 \n");
+		System.out.format("Class: %-16s | Time: %8s", "Functions", "2:00-3:15 \n");
+		
+		
+		
+		
+		
+		//System.out.format("%-15sTime: %8s", "Monday", "9:00-10:15 \n");
+		//System.out.format("%-15sTime: %8s", "Monday", "10:20-11:40 \n");
+		//System.out.format("%-15sTime: %8s", "Monday", "12:40-1:55 \n");
+		//System.out.format("%-15sTime: %8s", "Monday", "2:00-3:15 \n");
+	}
+
+}

5f731966b57e5a8a7a323a14f2a771cbf9441099
343033429 <343033429@MXL1502T7G.tdsb.on.ca>
2024-11-13 10:13:46 -0500

    If Challenge 3, not finished

---
+++ ICS3U-StewartK/src/unit2/IfChallenge3.java
@@ -0,0 +1,27 @@
+package unit2;
+import java.util.Scanner;
+
+/**
+ * Description: If challenge 3
+ * Date: November 13, 2024
+ * @author Kenny Stewart
+ */
+
+public class IfChallenge3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ready, answer;
+		int rights, wrongs;
+		
+		System.out.print("Are you ready to start a quiz? Press <Enter> when you're ready.");
+		ready = sc.nextLine();
+		System.out.println("Ok, here it comes!");
+		
+		System.out.println("\nQ1) Who thought of the law of disuse and use?");
+		System.out.println("\t1) Darwin");
+		System.out.println("\t2) Lamarck");
+		System.out.println("\t3) Lyell & Hutton");
+		System.out.println("\t4) Kenny");
+		
+	}
+}
